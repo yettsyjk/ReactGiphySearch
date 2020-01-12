9959c9aa4311be03c8d46c8a3fa8b1e8fadd19f4
yettsyjk <56506858+yettsyjk@users.noreply.github.com>
2020-01-11 19:51:31 -0700

    create SearchContainer component

---
+++ react-giphy-search/src/SearchContainer/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class SearchContainer extends Component {
+    constructor() {
+        super();
+    this.state = { term: ''};
+}
+render() {
+    return (
+<div className="SearchContainer">
+    <h1>Connecting SearchContainer</h1>
+</div>
+    );
+}
+}
+
+
+export default SearchContainer;
